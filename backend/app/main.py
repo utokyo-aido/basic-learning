55853ad52bb1424c50db2cf2c43c1a47a21b2f29
Yusuke Ikoma <shengjuyongcheng@gmail.com>
2025-06-28 20:21:57 +0900

    コメントアウトの消去

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # CORSの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Viteの開発サーバーのオリジン
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
